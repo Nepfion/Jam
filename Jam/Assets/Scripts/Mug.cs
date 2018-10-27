42a751d55380d4204d4b9f2447395a91b0be39d6
Nepfion <Nepfion96@gmail.com>
2018-10-27 12:10:08 +0300

    Added sprite scene

---
+++ Jam/Assets/Scripts/Mug.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Buffs
+{
+    Health,
+    Stamina,
+    Damage
+};
+
+public class Mug : MonoBehaviour {
+
+    public Buffs Buff;
+	// Use this for initialization
+	void Start () {
+        Buff = (Buffs)Random.Range(0, 2);
+        SpriteRenderer renderer = GetComponent<SpriteRenderer>();
+        switch(Buff)
+        {
+            case Buffs.Health:
+                renderer.color = Color.green;
+                break;
+            case Buffs.Stamina:
+                renderer.color = Color.yellow;
+                break;
+            case Buffs.Damage:
+                renderer.color = Color.red;
+                break;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Jam/Assets/Scripts/Table.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Table : MonoBehaviour {
+
+    public GameObject Mug;
+    private List<GameObject> mugs;
+    
+	void Start () {
+        mugs = new List<GameObject>();
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            mugs.Add(createMug());
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown(0))
+        {
+            for (int i = 0; i < mugs.Count; i++)
+            {
+                Destroy(mugs[i]);
+                mugs[i] = createMug();
+            }
+        }
+	}
+
+    private GameObject createMug()
+    {
+        int posIndex = Random.Range(0, transform.childCount - 1);
+
+        Vector3 mugTransform = transform.GetChild(posIndex).transform.position;
+        return Instantiate(Mug, mugTransform, Quaternion.identity);
+    }
+}
