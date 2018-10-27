e86e64f7a418f1e6c61039cde84e5ad3d814f054
Psarev <nikitapsarev@gmail.com>
2018-10-27 12:09:30 +0300

    Add basic top-down shooter functionality

---
+++ Jam/Assets/EnemyMovement.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyMovement : MonoBehaviour
+{
+    Transform player;
+    PlayerHealth playerHealth;
+    EnemyHealth enemyHealth;
+    NavMeshAgent nav;
+
+    private void Awake()
+    {
+        player = GameObject.FindWithTag("Player").transform;
+        playerHealth = player.GetComponent<PlayerHealth>();
+        enemyHealth = GetComponent<EnemyHealth>();
+        nav = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        if (enemyHealth.CurrentHealth > 0 && playerHealth.CurrentHealth > 0)
+        {
+            nav.SetDestination(player.position);
+        }
+        else
+        {
+            nav.enabled = false;
+        }
+    }
+
+}
